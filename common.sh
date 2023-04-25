a3d8f84850378e6f1dcfce6cc5270031aa007959
Pranathi Alapati <tpranathi04@gmail.com>
2023-04-25 13:40:59 +0530

    payment code dry

--- common.sh
+++ common.sh
@@ -144,5 +144,6 @@ func_python()
   func_print_head "Update passwords in system service file"
   sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" $script_path/payment.service &>>$log_file
   func_status_check $?
+
   func_systemd_setup
 }
