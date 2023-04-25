c6cdcfca11e739240149ff659fcffb5800b7abb8
Pranathi Alapati <tpranathi04@gmail.com>
2023-04-25 11:38:28 +0530

    changing mysql

--- mysql.sh
+++ mysql.sh
@@ -3,23 +3,29 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
 
-
 if [ -z "$mysql_root_password" ];then
   echo Input mysql_root_password password missing
   exit
 fi
 
-echo -e "\e[36m>>>>>>> Disable mysql old version<<<<<<<<\e[0m"
-dnf module disable mysql -y
-echo -e "\e[36m>>>>>>> copy mysql repo file<<<<<<<<\e[0m"
-cp $script_path/mysql.repo /etc/yum.repos.d/mysql.repo
-echo -e "\e[36m>>>>>>> install mysql<<<<<<<<\e[0m"
-yum install mysql-community-server -y
+func_print_head "Disable mysql old version"
+dnf module disable mysql -y &>>$log_file
+func_status_check $?
 
-echo -e "\e[36m>>>>>>> start mysql<<<<<<<<\e[0m"
-systemctl enable mysqld
-systemctl start mysqld
+func_print_head "copy mysql repo file"
+cp $script_path/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+func_status_check $?
+
+func_print_head "install mysql"
+yum install mysql-community-server -y &>>$log_file
+func_status_check $?
+
+func_print_head "start mysql"
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
+func_status_check $?
 
 echo -e "\e[36m>>>>>>> set mysql password<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass ${mysql_root_password}
 mysql -uroot -p${mysql_root_password}
+func_status_check $?
