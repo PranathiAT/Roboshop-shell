518c5ccafaa1a45fc5e83410f20ebf7597ce53e7
Pranathi Alapati <tpranathi04@gmail.com>
2023-04-25 13:31:55 +0530

    payment code dry

--- payment.sh
+++ payment.sh
@@ -3,11 +3,11 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 rabbitmq_appuser_password=$1
 component=payment
-func_python
+
 
 if [ -z "$rabbitmq_appuser_password" ];then
   echo Input Rabbitmq app user paswword missing
   exit
 fi
 
-
+func_python
