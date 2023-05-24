b58dffb17b17ea58f854ad6957d1b162eb45f802
Pranathi Alapati <tpranathi04@gmail.com>
2023-05-24 16:58:19 +0530

    raqbbitmq.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -18,9 +18,9 @@ func_print_head "set up rabbitmq repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log_file
 func_status_check $?
 
-func_print_head "install erlang"
-yum install erlang  -y &>>$log_file
-func_status_check $?
+#func_print_head "install erlang"
+#yum install erlang  -y &>>$log_file
+#func_status_check $?
 
 func_print_head "install rabbitmq"
 yum install rabbitmq-server  -y &>>$log_file
